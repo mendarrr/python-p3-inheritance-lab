47922bc5feeb438d12b67893981f941e7d585e23
mendarrr <sarahabby541@gmail.com>
2024-05-27 20:04:13 +0300

    Completed lab

--- lib/user.py
+++ lib/user.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python
 
 class User:
+    def __init__(self, first_name, last_name, knowledge=None):
+        self.first_name = first_name
+        self.last_name = last_name
+        if knowledge is None:
+            self.knowledge = []
+        else:
+            self.knowledge = knowledge
+
     pass
